78d0849fc8a10e91fcba0f921f52b80feba3fa55
Nikola G <nikolagav4@gmail.com>
2023-02-24 12:04:35 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+search_header = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'cookie': '',                         # Put your account cookies here
+    'referer': 'https://www.google.com/',
+    'user-agent': 'Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5082.113 Safari/537.36'
+}
+
+post_header = {
+    'accept': 'application/vnd.linkedin.normalized+json+2.1',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/json; charset=UTF-8',
+    'cookie': '',                         # Put your account cookies here
+    'csrf-token': 'ajax:3424180731116273503',
+    'origin': 'https://www.linkedin.com',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5145.138 Safari/537.36',
+    'x-li-lang': 'en_US',
+    'x-li-page-instance': 'urn:li:page:d_flagship3_search_srp_people_load_more;6itmNAJOQ8GvfpHbJbHwjw==',
+    'x-restli-protocol-version': '2.0.0'
+    }
---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import time
+from config import *
+
+
+check = False
+pages = 0
+users = []
+user_id = ''     #Enter the user id of profile that you want to mutually connect with
+
+while check == False:
+    pages += 1
+    connects = 0
+    url = f'https://www.linkedin.com/search/results/people/?connectionOf=%5B%22{user_id}%22%5D&network=%5B%22F%22%2C%22S%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH&sid=v2g'
+    response = requests.get(url, headers = search_header)
+
+    if 'fsd_profileActions:' not in response.text:
+        print('no more pages')
+        pages -= 1
+        check = True
+    elif 'fsd_profileActions:' in response.text:
+        id = response.text.split('fsd_profileActions:(')[pages].split(',')[0]
+        if id not in users:
+            amount = response.text.count('fsd_profileActions:(')
+
+            for i in range(amount):
+                id = response.text.split('fsd_profileActions:(')[i +1].split(',')[0]
+                if id not in users:
+                    users.append(id)
+
+            for user in users:
+                url = 'https://www.linkedin.com/voyager/api/voyagerRelationshipsDashMemberRelationships?action=verifyQuotaAndCreate'
+
+                data = {
+                    'inviteeProfileUrn': f"urn:li:fsd_profile:{user}"
+                }
+
+                response = requests.post(url = url, headers = post_header, json = data)
+                if 'included' in response.text:
+                    connects += 1
+                    print(f'followed: {connects}')
+                    time.sleep(3)
+            users = []
+
+
+print(f'pages: {pages}')
